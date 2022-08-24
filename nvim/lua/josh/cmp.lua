8d59594afc03ae0a819adeedda78fde495d17544
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-08-24 13:51:31 -0700

    closes #2

--- nvim/lua/josh/cmp.lua
+++ nvim/lua/josh/cmp.lua
@@ -1,73 +1,6 @@
 local cmp = require('cmp')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
-local ok, Job = pcall(require, 'plenary.job')
-
-if ok then
-  local source = {}
-
-  source.new = function()
-    return setmetatable({ cache = {} }, { __index = source })
-  end
-
-  source.complete = function(self, _, callback)
-    local bufnr = vim.api.nvim_get_current_buf()
-    if self.cache[bufnr] then
-      callback({ items = self.cache[bufnr], isIncomplete = false })
-    else
-      Job
-        :new({
-          'gh',
-          'issue',
-          'list',
-          '--limit',
-          '1000',
-          '--json',
-          'body,number,title',
-          on_exit = function(job)
-            local result = job:result()
-            local _ok, decoded =
-              pcall(vim.json.decode, table.concat(result, ''))
-            if _ok then
-              local items = {}
-              for _, item in ipairs(decoded) do
-                table.insert(items, {
-                  label = string.format('#%s', item.number),
-                  documentation = {
-                    kind = 'markdown',
-                    value = string.format(
-                      '# %s\n\n%s',
-                      item.title,
-                      string.gsub(item.body or '', '\r', '')
-                    ),
-                  },
-                })
-              end
-              callback({ items = items, isIncomplete = false })
-              self.cache[bufnr] = items
-            else
-              vim.notify('Unable to decode response.')
-            end
-          end,
-        })
-        :start()
-    end
-  end
-
-  source.get_trigger_characters = function()
-    return { '#' }
-  end
-
-  source.is_available = function()
-    return vim.tbl_contains(
-      { 'gitcommit', 'NeogitCommitMessage' },
-      vim.bo.filetype
-    )
-  end
-
-  cmp.register_source('gh_issues', source.new())
-end
-
 local offset = 4
 
 cmp.setup({
@@ -89,7 +22,7 @@ cmp.setup({
     end,
   },
   sources = cmp.config.sources({
-    { name = 'gh_issues' },
+    { name = 'git' },
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
     { name = 'luasnip' },
