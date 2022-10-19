dc72b5ff33867baaf15f69a833e011c682e8dfa5
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-10-19 12:03:27 -0700

    fix packer bootstrap

--- nvim/lua/josh/lsp.lua
+++ nvim/lua/josh/lsp.lua
@@ -20,10 +20,6 @@ local keymaps = {
   gy = vim.lsp.buf.type_definition,
 }
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
-
 local function on_attach(client, bufnr)
   for k, v in pairs(keymaps) do
     vim.keymap.set('n', k, v, { buffer = bufnr })
@@ -35,6 +31,7 @@ local servers = vim.list_extend(
   { 'ccls' }
 )
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 for _, server in ipairs(servers) do
   local opts = {
     capabilities = capabilities,
