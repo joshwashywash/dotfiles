a71a82c3bc875537712b177d2223b540c42a778e
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-11-14 12:21:33 -0800

    fix really old keymap
    i wonder how long that's been in there???

--- nvim/lua/josh/telescope.lua
+++ nvim/lua/josh/telescope.lua
@@ -5,13 +5,11 @@ require('telescope').setup({
   defaults = {
     mappings = {
       i = {
-        ['<c-h>'] = actions.move_selection_previous,
-        ['<c-k>'] = actions.move_selection_next,
         ['<c-t>'] = trouble.open_with_trouble,
       },
       n = {
         ['<c-t>'] = trouble.open_with_trouble,
-        h = actions.move_selection_previous,
+        j = actions.move_selection_previous,
         k = actions.move_selection_next,
       },
     },
