759b470621032d3924b6c837ee3f1cbc78393279
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-12-16 13:46:20 -0800

    Update null-ls.lua
    add luasnip and remove clang-format. i don't use c/c++

--- nvim/lua/josh/null-ls.lua
+++ nvim/lua/josh/null-ls.lua
@@ -23,7 +23,7 @@ null_ls.setup({
   end,
   sources = {
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.completion.luasnip,
     null_ls.builtins.formatting.dart_format,
     null_ls.builtins.formatting.prettier.with({
       extra_filetypes = { 'svelte', 'toml' },
