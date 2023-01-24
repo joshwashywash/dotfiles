9d0fd49a873da319b39b3e8cadade516b71df4c5
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-01-23 18:59:43 -0800

    remove flit

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,12 +1,7 @@
 return {
   'ggandor/leap.nvim',
-  config = function(_, opts)
-    local leap = require('leap')
-    for k, v in pairs(opts) do
-      leap.opts[k] = v
-    end
-    leap.add_default_mappings(true)
+  config = function()
+    require('leap').add_default_mappings(true)
   end,
-  dependencies = { { 'ggandor/flit.nvim', opts = { labeled_modes = 'nv' } } },
   event = 'VeryLazy',
 }
