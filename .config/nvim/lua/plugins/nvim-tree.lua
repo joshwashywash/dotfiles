b644f0f0626633ecc0a75d05900d8f7385000d4b
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-03-14 14:46:15 -0700

    update to nvimtree's new way to do keymaps

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,44 +1,55 @@
-local function create_keymap(keymap)
-  local key, action = unpack(keymap)
-  return { key = key, action = action }
-end
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
 
-local keymaps = {
-  { '..', 'dir_up' },
-  { './', 'cd' },
-  { '<bs>', 'close_node' },
-  { '<c-d>', 'toggle_dotfiles' },
-  { '<c-r>', 'refresh' },
-  { '<c-y>', 'copy_absolute_path' },
-  { '<cr>', 'edit' },
-  { '<tab>', 'preview' },
-  { 'C', 'collapse_all' },
-  { 'D', 'trash' },
-  { 'E', 'expand_all' },
-  { 'I', 'toggle_ignored' },
-  { 'K', 'toggle_file_info' },
-  { 'P', 'parent_node' },
-  { 'R', 'full_rename' },
-  { 'Y', 'copy_path' },
-  { 'a', 'create' },
-  { 'c', 'copy' },
-  { 'd', 'remove' },
-  { 'g?', 'toggle_help' },
-  { 'gj', 'first_sibling' },
-  { 'gk', 'last_sibling' },
-  { 'gn', 'next_git_item' },
-  { 'gp', 'prev_git_item' },
-  { 'h', 'split' },
-  { 'mm', 'toggle_mark' },
-  { 'mp', 'bulk_move' },
-  { 'o', 'system_open' },
-  { 'p', 'paste' },
-  { 'r', 'rename' },
-  { 'v', 'vsplit' },
-  { 'x', 'cut' },
-  { 'y', 'copy_name' },
-  { { 'q', '<esc>' }, 'close' },
-}
+  local keymaps = {
+    { 'n', '..', api.tree.change_root_to_parent, 'up' },
+    { 'n', './', api.tree.change_root_to_node, 'cd' },
+    { 'n', '<bs>', api.node.navigate.parent_close, 'close directory' },
+    { 'n', '<c-d>', api.tree.toggle_hidden_filter, 'toggle dotfiles' },
+    { 'n', '<c-r>', api.tree.reload, 'refresh' },
+    { 'n', '<c-y>', api.fs.copy.absolute_path, 'copy absolute path' },
+    { 'n', '<cr>', api.node.open.edit, 'open' },
+    { 'n', '<esc>', api.tree.close, 'close' },
+    { 'n', '<tab>', api.node.open.preview, 'open preview' },
+    { 'n', 'C', api.tree.collapse_all, 'collapse' },
+    { 'n', 'D', api.fs.trash, 'trash' },
+    { 'n', 'E', api.tree.expand_all, 'expand all' },
+    { 'n', 'I', api.tree.toggle_gitignore_filter, 'toggle git ignore' },
+    { 'n', 'K', api.node.show_info_popup, 'info' },
+    { 'n', 'P', api.node.navigate.parent, 'parent directory' },
+    { 'n', 'R', api.fs.rename_sub, 'rename: omit filename' },
+    { 'n', 'Y', api.fs.copy.relative_path, 'copy relative path' },
+    { 'n', 'a', api.fs.create, 'create' },
+    { 'n', 'c', api.fs.copy.node, 'copy' },
+    { 'n', 'd', api.fs.remove, 'delete' },
+    { 'n', 'g?', api.tree.toggle_help, 'help' },
+    { 'n', 'gj', api.node.navigate.sibling.first, 'first sibling' },
+    { 'n', 'gk', api.node.navigate.sibling.last, 'last sibling' },
+    { 'n', 'gn', api.node.navigate.git.next, 'next git' },
+    { 'n', 'gp', api.node.navigate.git.prev, 'prev git' },
+    { 'n', 'h', api.node.open.horizontal, 'open with horz split' },
+    { 'n', 'mm', api.marks.toggle, 'toggle bookmark' },
+    { 'n', 'mp', api.marks.bulk.move, 'move bookmarked' },
+    { 'n', 'o', api.node.run.system, 'run system' },
+    { 'n', 'p', api.fs.paste, 'paste' },
+    { 'n', 'q', api.tree.close, 'close' },
+    { 'n', 'r', api.fs.rename, 'rename' },
+    { 'n', 'v', api.node.open.vertical, 'open with vert split' },
+    { 'n', 'x', api.fs.cut, 'cut' },
+    { 'n', 'y', api.fs.copy.filename, 'copy name' },
+  }
+
+  for _, value in pairs(keymaps) do
+    local left, right, fn, desc = unpack(value)
+    vim.keymap.set(left, right, fn, {
+      desc = 'nvim-tree: ' .. desc,
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      nowait = true,
+    })
+  end
+end
 
 return {
   'kyazdani42/nvim-tree.lua',
@@ -46,12 +57,16 @@ return {
   keys = {
     {
       '<leader>ef',
-      '<cmd>NvimTreeFindFile<cr>',
+      function()
+        vim.cmd('NvimTreeFindFile')
+      end,
       desc = 'find file in explorer',
     },
     {
       '<leader>et',
-      '<cmd>NvimTreeToggle<cr>',
+      function()
+        vim.cmd('NvimTreeToggle')
+      end,
       desc = 'toggle explorer',
     },
   },
@@ -66,7 +81,7 @@ return {
         },
       },
     },
-    hijack_cursor = true,
+    on_attach = on_attach,
     renderer = {
       icons = {
         show = {
@@ -77,7 +92,6 @@ return {
     view = {
       mappings = {
         custom_only = true,
-        list = vim.tbl_map(create_keymap, keymaps),
       },
       side = 'right',
     },
