0670eb9f9c457cdee200ddef5862813d26614a42
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-03-24 09:34:39 -0700

    add resume find and dry

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local prefix = '<leader>f';
+
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
@@ -21,11 +23,11 @@ return {
     })
 
     local keymaps = {
-      { 'n', '<leader>f:', builtin.command_history, 'command history' },
-      { 'n', '<leader>f*', builtin.grep_string, 'word under cursor' },
+      { 'n', ':', builtin.command_history, 'command history' },
+      { 'n', '*', builtin.grep_string, 'word under cursor' },
       {
         'n',
-        '<leader>f/',
+        '/',
         function()
           builtin.current_buffer_fuzzy_find(
             require('telescope.themes').get_dropdown({ previewer = false })
@@ -33,30 +35,31 @@ return {
         end,
         'in current buffer',
       },
-      { 'n', '<leader>fC', builtin.colorscheme, 'colorschemes' },
-      { 'n', '<leader>fG', builtin.git_commits, 'git commits' },
-      { 'n', '<leader>fH', builtin.highlights, 'highlights' },
-      { 'n', '<leader>fa', builtin.autocommands, 'autocommands' },
-      { 'n', '<leader>fb', builtin.buffers, 'buffers' },
-      { 'n', '<leader>fc', builtin.commands, 'commands' },
-      { 'n', '<leader>ff', builtin.find_files, 'files' },
-      { 'n', '<leader>fg', builtin.live_grep, 'find in files' },
-      { 'n', '<leader>fh', builtin.help_tags, 'help tags' },
-      { 'n', '<leader>fk', builtin.keymaps, 'keymaps' },
-      { 'n', '<leader>fm', builtin.marks, 'marks' },
-      { 'n', '<leader>fr', builtin.oldfiles, 'recent files' },
-      { 'n', '<leader>fs', builtin.git_status, 'git status' },
+      { 'n', 'C', builtin.colorscheme, 'colorschemes' },
+      { 'n', 'G', builtin.git_commits, 'git commits' },
+      { 'n', 'H', builtin.highlights, 'highlights' },
+      { 'n', 'L', builtin.resume, 'resume last search' },
+      { 'n', 'a', builtin.autocommands, 'autocommands' },
+      { 'n', 'b', builtin.buffers, 'buffers' },
+      { 'n', 'c', builtin.commands, 'commands' },
+      { 'n', 'f', builtin.find_files, 'files' },
+      { 'n', 'g', builtin.live_grep, 'find in files' },
+      { 'n', 'h', builtin.help_tags, 'help tags' },
+      { 'n', 'k', builtin.keymaps, 'keymaps' },
+      { 'n', 'm', builtin.marks, 'marks' },
+      { 'n', 'r', builtin.oldfiles, 'recent files' },
+      { 'n', 's', builtin.git_status, 'git status' },
     }
 
     for _, keymap in ipairs(keymaps) do
       local mode, l, r, desc = unpack(keymap)
-      vim.keymap.set(mode, l, r, { desc = desc })
+      vim.keymap.set(mode, prefix .. l, r, { desc = desc })
     end
   end,
   cmd = {
     'Telescope'
   },
   keys = {
-    '<leader>f'
+    prefix,
   },
 }
