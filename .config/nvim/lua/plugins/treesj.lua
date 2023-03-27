f0765c45538480286864211393675355899feed7
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-03-27 08:36:47 -0700

    missed actually adding treesj.lua

---
+++ .config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,28 @@
+return {
+  'Wansmer/treesj',
+  config = { use_default_keymaps = false },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  keys = {
+    {
+      '<space>jj',
+      function()
+        require('treesj').join()
+      end,
+      desc = 'join',
+    },
+    {
+      '<space>js',
+      function()
+        require('treesj').split()
+      end,
+      desc = 'split',
+    },
+    {
+      '<space>jt',
+      function()
+        require('treesj').toggle()
+      end,
+      desc = 'toggle',
+    },
+  },
+}
