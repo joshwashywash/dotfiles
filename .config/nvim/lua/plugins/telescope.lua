6b6a15fd1942aeafb7c996340df99cb3f12b1d99
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-05-11 23:37:57 -0700

    make telescope preview bigger

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
 
     telescope.setup({
       defaults = {
+        layout_config = { horizontal = { preview_width = 0.6 } },
         selection_caret = '  ',
         prompt_prefix = '  ',
         mappings = {
@@ -21,7 +22,17 @@ return {
           },
         },
       },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = 'smart_case',
+        },
+      },
     })
+
+    require('telescope').load_extension('fzf')
 
     local normal_mode_keymaps = {
       { '*', builtin.grep_string, 'word under cursor' },
@@ -66,7 +77,10 @@ return {
   cmd = {
     'Telescope',
   },
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
   keys = {
     prefix,
   },
