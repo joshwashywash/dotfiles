2a8c45efcb98c638b4abc2817546c397b1aac676
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2023-12-10 12:54:03 -0800

    use init for colorscheme

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -1,15 +1,14 @@
-local rose_pine_name = 'rose-pine'
+local name = 'rose-pine'
 
 return {
 	'rose-pine/neovim',
-	config = function(_, opts)
-		require(rose_pine_name).setup(opts)
-		vim.cmd.colorscheme(rose_pine_name)
+	init = function()
+		vim.cmd.colorscheme(name)
 	end,
+	lazy = false,
+	name = name,
 	opts = {
 		dark_variant = 'moon',
 	},
-	lazy = false,
-	name = rose_pine_name,
 	priority = 1000,
 }
