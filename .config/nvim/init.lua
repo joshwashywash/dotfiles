50047346506326172ac5408580f9669bcd857023
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2023-12-16 21:25:01 -0800

    fix hi and clue

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = ' '
 
--- MAPS
 vim.keymap.set('n', '<s-down>', '<c-w>j', { desc = 'go to lower window' })
 vim.keymap.set('n', '<s-left>', '<c-w>h', { desc = 'go to left window' })
 vim.keymap.set('n', '<s-right>', '<c-w>l', { desc = 'go to right window' })
@@ -11,26 +10,45 @@ vim.keymap.set('n', '<leader>bq', '<cmd>lua MiniBufremove.delete()<cr>', { desc 
 vim.keymap.set('n', '<leader>bw', '<cmd>lua MiniBufremove.wipeout()<cr>', { desc = 'wipeout' })
 
 -- files
-vim.keymap.set('n', '<leader>f', '<cmd>lua MiniFiles.open()<cr>', { desc = 'open file explorer' })
+vim.keymap.set('n', '<leader>f', '<cmd>lua MiniFiles.open()<cr>', { desc = 'open files' })
 
 -- pick
+vim.keymap.set('n', '<leader>pC', '<cmd>Pick cli<cr>', { desc = 'cli' })
+vim.keymap.set('n', '<leader>pF', '<cmd>Pick oldfiles<cr>', { desc = 'recent files' })
+vim.keymap.set('n', '<leader>pG', '<cmd>Pick hl_groups<cr>', { desc = 'hl groups' })
+vim.keymap.set('n', '<leader>pH', '<cmd>Pick help<cr>', { desc = 'help' })
+vim.keymap.set('n', '<leader>pR', '<cmd>Pick resume<cr>', { desc = 'resume' })
 vim.keymap.set('n', '<leader>pb', '<cmd>Pick buffers<cr>', { desc = 'buffers' })
-vim.keymap.set('n', '<leader>pc', '<cmd>Pick cli<cr>', { desc = 'cli' })
+vim.keymap.set('n', '<leader>pc', '<cmd>Pick commands<cr>', { desc = 'comands' })
+vim.keymap.set(
+	'n',
+	'<leader>pd',
+	"<cmd>Pick diagnostic scope='current'<cr>",
+	{ desc = 'diagnostic' }
+)
+vim.keymap.set('n', '<leader>pe', '<cmd>Pick explorer<cr>', { desc = 'explorer' })
 vim.keymap.set('n', '<leader>pf', '<cmd>Pick files<cr>', { desc = 'files' })
 vim.keymap.set('n', '<leader>pg', '<cmd>Pick grep_live<cr>', { desc = 'grep_live' })
-vim.keymap.set('n', '<leader>ph', '<cmd>Pick help<cr>', { desc = 'help' })
-vim.keymap.set('n', '<leader>pr', '<cmd>Pick resume<cr>', { desc = 'resume' })
+vim.keymap.set('n', '<leader>ph', '<cmd>Pick history<cr>', { desc = 'history' })
+vim.keymap.set('n', '<leader>pk', '<cmd>Pick keymaps<cr>', { desc = 'keymaps' })
+vim.keymap.set('n', '<leader>pm', '<cmd>Pick marks<cr>', { desc = 'marks' })
+vim.keymap.set('n', '<leader>po', '<cmd>Pick options<cr>', { desc = 'options' })
+vim.keymap.set('n', '<leader>pp', '<cmd>Pick hipatterns<cr>', { desc = 'hi patterns' })
+vim.keymap.set('n', '<leader>pr', '<cmd>Pick registers<cr>', { desc = 'registers' })
+vim.keymap.set('n', '<leader>pt', '<cmd>Pick treesitter<cr>', { desc = 'treesitter' })
+vim.keymap.set('n', '<leader>pv', '<cmd>Pick visit_paths<cr>', { desc = 'visits' })
 
--- OPTS
+vim.opt.cmdheight = 0
 vim.opt.fillchars = { eob = ' ' }
 vim.opt.incsearch = true
+vim.opt.pumheight = 10
 vim.opt.shiftwidth = 2
-vim.opt.splitbelow = true
-vim.opt.tabstop = 2
-vim.opt.signcolumn = 'yes'
 vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.splitbelow = true
+vim.opt.statusline = '%f %m %= %l:%c ♥'
+vim.opt.tabstop = 2
 
--- UI
 vim.diagnostic.config({
 	virtual_text = false,
 })
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -5,11 +5,57 @@ return {
 		require('mini.ai').setup()
 		require('mini.bracketed').setup()
 		require('mini.bufremove').setup()
-		require('mini.clue').setup()
+		local clue = require('mini.clue')
+		clue.setup({
+			clues = {
+				{ mode = 'n', keys = '<leader>b', desc = 'buffer' },
+				{ mode = 'n', keys = '<leader>p', desc = 'pick' },
+				-- Enhance this by adding descriptions for <Leader> mapping groups
+				clue.gen_clues.builtin_completion(),
+				clue.gen_clues.g(),
+				clue.gen_clues.marks(),
+				clue.gen_clues.registers(),
+				clue.gen_clues.windows(),
+				clue.gen_clues.z(),
+			},
+			triggers = {
+
+				-- Leader triggers
+				{ mode = 'n', keys = '<Leader>' },
+				{ mode = 'x', keys = '<Leader>' },
+
+				-- Built-in completion
+				{ mode = 'i', keys = '<C-x>' },
+
+				-- `g` key
+				{ mode = 'n', keys = 'g' },
+				{ mode = 'x', keys = 'g' },
+
+				-- Marks
+				{ mode = 'n', keys = "'" },
+				{ mode = 'n', keys = '`' },
+				{ mode = 'x', keys = "'" },
+				{ mode = 'x', keys = '`' },
+
+				-- Registers
+				{ mode = 'n', keys = '"' },
+				{ mode = 'x', keys = '"' },
+				{ mode = 'i', keys = '<C-r>' },
+				{ mode = 'c', keys = '<C-r>' },
+
+				-- Window commands
+				{ mode = 'n', keys = '<C-w>' },
+
+				-- `z` key
+				{ mode = 'n', keys = 'z' },
+				{ mode = 'x', keys = 'z' },
+			},
+		})
 		require('mini.comment').setup()
 		require('mini.completion').setup()
 		require('mini.cursorword').setup()
-		require('mini.extra').setup()
+		local extra = require('mini.extra')
+		extra.setup()
 		require('mini.files').setup({
 			mappings = {
 				close = '<esc>',
@@ -20,7 +66,18 @@ return {
 			},
 		})
 		require('mini.fuzzy').setup()
-		require('mini.hipatterns').setup()
+		local hi_words = extra.gen_highlighter.words
+		local hipatterns = require('mini.hipatterns')
+		hipatterns.setup({
+			highlighters = {
+				fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+				hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+				todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+				note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
+
 		require('mini.jump').setup()
 		require('mini.move').setup({
 			mappings = {
