fa93864d22c4a54736a38a0a42d5cd563320d6ad
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-01-01 12:47:01 -0800

    add incremental selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,15 @@ return {
 				'vim',
 				'vimdoc',
 			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = '<leader>n',
+					node_incremental = 'n',
+					node_decremental = 'N',
+					scope_incremental = false,
+				},
+			},
 			highlight = {
 				additional_vim_regex_highlighting = false,
 				enable = true,
