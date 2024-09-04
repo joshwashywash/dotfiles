78aada6cf2e87868ac5a365b8c2e6c043742d0be
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-09-04 13:53:18 -0700

    use new split window files api

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -376,17 +376,16 @@ later(function()
 		},
 	})
 
-	-- create keymaps for creating splits when mini files is open
 	local map_split = function(buf_id, lhs, direction)
 		local rhs = function()
 			-- Make new window and set it as target
-			local new_target_window
-			vim.api.nvim_win_call(files.get_target_window(), function()
+			local cur_target = files.get_explorer_state().target_window
+			local new_target = vim.api.nvim_win_call(cur_target, function()
 				vim.cmd(direction .. ' split')
-				new_target_window = vim.api.nvim_get_current_win()
+				return vim.api.nvim_get_current_win()
 			end)
 
-			files.set_target_window(new_target_window)
+			files.set_target_window(new_target)
 		end
 
 		-- Adding `desc` will result into `show_help` entries
@@ -398,8 +397,8 @@ later(function()
 		pattern = 'MiniFilesBufferCreate',
 		callback = function(args)
 			local buf_id = args.data.buf_id
-			map_split(buf_id, 'gs', 'belowright horizontal')
-			map_split(buf_id, 'gv', 'belowright vertical')
+			map_split(buf_id, '<c-s>', 'belowright horizontal')
+			map_split(buf_id, '<c-v>', 'belowright vertical')
 		end,
 	})
 
