acb2cf80e52f79eda2e4c77e42b21583b62ef057
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-11-13 19:04:22 -0800

    just use lspattach autocmd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -650,6 +650,14 @@ later(function()
 	---@type {mode: string|string[], lhs_suffix_key: string, rhs: string|function, opts: vim.keymap.set.Opts}[]
 	local keymaps = {
 		{
+			mode = { 'n', 'v' },
+			lhs_suffix_key = 'a',
+			rhs = vim.lsp.buf.code_action,
+			opts = {
+				desc = 'code action',
+			},
+		},
+		{
 			mode = 'n',
 			lhs_suffix_key = 'R',
 			rhs = function()
@@ -661,6 +669,16 @@ later(function()
 		},
 		{
 			mode = 'n',
+			lhs_suffix_key = 'I',
+			rhs = function()
+				vim.cmd('check lspconfig')
+			end,
+			opts = {
+				desc = 'info',
+			},
+		},
+		{
+			mode = 'n',
 			lhs_suffix_key = 'r',
 			rhs = vim.lsp.buf.rename,
 			opts = {
@@ -668,27 +686,35 @@ later(function()
 			},
 		},
 		{
-			mode = { 'n', 'v' },
-			lhs_suffix_key = 'a',
-			rhs = vim.lsp.buf.code_action,
+			mode = 'n',
+			lhs_suffix_key = 'S',
+			rhs = function()
+				vim.cmd('LspStop')
+			end,
 			opts = {
-				desc = 'code action',
+				desc = 'stop',
 			},
 		},
 	}
 
-	---@param buf_id number
-	---@param client vim.lsp.Client
-	local on_attach = function(client, buf_id)
-		vim.bo[buf_id].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
-		-- let conform handle formatting
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentRangeFormattingProvider = false
-		for _, v in ipairs(keymaps) do
-			v.opts.buffer = buf_id
-			vim.keymap.set(v.mode, '<leader>l' .. v.lhs_suffix_key, v.rhs, v.opts)
-		end
-	end
+	vim.api.nvim_create_autocmd('LspAttach', {
+		callback = function(event)
+			local bufnr = event.buf
+			local client = vim.lsp.get_client_by_id(event.data.client_id)
+			if client then
+				if client.server_capabilities.completionProvider then
+					vim.bo[bufnr].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+				end
+				-- let conform handle formatting
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end
+			for _, v in ipairs(keymaps) do
+				v.opts.buffer = bufnr
+				vim.keymap.set(v.mode, '<leader>l' .. v.lhs_suffix_key, v.rhs, v.opts)
+			end
+		end,
+	})
 
 	require('mason').setup({
 		ui = {
@@ -709,13 +735,10 @@ later(function()
 		},
 		handlers = {
 			function(server_name)
-				lsp[server_name].setup({
-					on_attach = on_attach,
-				})
+				lsp[server_name].setup({})
 			end,
 			gopls = function()
 				lsp.gopls.setup({
-					on_attach = on_attach,
 					settings = {
 						analyses = {
 							unusedparams = true,
@@ -726,7 +749,6 @@ later(function()
 			end,
 			denols = function()
 				lsp.denols.setup({
-					on_attach = on_attach,
 					root_dir = lsp.util.root_pattern('deno.json', 'deno.jsonc'),
 				})
 			end,
@@ -735,7 +757,6 @@ later(function()
 				local capabilities = vim.lsp.protocol.make_client_capabilities()
 				capabilities.textDocument.completion.completionItem.snippetSupport = true
 				lsp.jsonls.setup({
-					on_attach = on_attach,
 					capabilities = capabilities,
 					settings = {
 						json = {
@@ -749,7 +770,6 @@ later(function()
 			end,
 			lua_ls = function()
 				lsp.lua_ls.setup({
-					on_attach = on_attach,
 					on_init = function(client)
 						if client.workspace_folders then
 							local path = client.workspace_folders[1].name
@@ -789,7 +809,6 @@ later(function()
 			end,
 			ts_ls = function()
 				lsp.ts_ls.setup({
-					on_attach = on_attach,
 					root_dir = lsp.util.root_pattern('package.json'),
 					single_file_support = false,
 				})
