bf68c36319109dc80de176b8639260d09ec12c7a
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-04-10 20:34:06 -0500

    add other files

---
+++ .config/nvim/plugins/nvim-lint.lua
@@ -0,0 +1,23 @@
+local lint = require('lint')
+
+local eslint = {
+	'eslint_d',
+}
+
+lint.linters_by_ft = {
+	javascript = eslint,
+	svelte = eslint,
+	typescript = eslint,
+}
+
+vim.api.nvim_create_user_command('TryLint', lint.try_lint, {
+	desc = 'try linting the buffer',
+})
+
+-- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
+-- 	callback = function()
+-- 		lint.try_lint()
+-- 	end,
+-- 	desc = 'lint on write',
+-- 	group = vim.api.nvim_create_augroup('lint', { clear = true }),
+-- })
