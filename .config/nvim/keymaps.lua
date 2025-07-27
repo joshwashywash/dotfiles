280cc26e7445d3d4d82c5f17711637e2a48537f4
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-07-26 19:37:28 -0500

    change window focus keymaps

--- .config/nvim/keymaps.lua
+++ .config/nvim/keymaps.lua
@@ -2,33 +2,28 @@
 local window_focus_keymaps = {
 	{
 		direction = 'lower',
-		lhs_suffix_key = 'down',
+		lhs_suffix_key = '<down>',
 		rhs_suffix_key = 'j',
 	},
 	{
 		direction = 'left',
-		lhs_suffix_key = 'left',
+		lhs_suffix_key = '<left>',
 		rhs_suffix_key = 'h',
 	},
 	{
 		direction = 'right',
-		lhs_suffix_key = 'right',
+		lhs_suffix_key = '<right>',
 		rhs_suffix_key = 'l',
 	},
 	{
 		direction = 'upper',
-		lhs_suffix_key = 'up',
+		lhs_suffix_key = '<up>',
 		rhs_suffix_key = 'k',
 	},
 }
 
 for _, v in ipairs(window_focus_keymaps) do
-	vim.keymap.set(
-		'n',
-		'<s-' .. v.lhs_suffix_key .. '>',
-		'<c-w>' .. v.rhs_suffix_key,
-		{
-			desc = 'focus' .. v.direction .. 'window',
-		}
-	)
+	vim.keymap.set('n', '<c-w>' .. v.lhs_suffix_key, '<c-w>' .. v.rhs_suffix_key, {
+		desc = 'focus ' .. v.direction .. ' window',
+	})
 end
