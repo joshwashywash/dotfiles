661edd0e25b7d4c966cfa5eb50e7be22e63bae38
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2025-11-03 19:27:16 -0600

    update treesitter config

--- .config/nvim/plugin/plugins/treesitter.lua
+++ .config/nvim/plugin/plugins/treesitter.lua
@@ -10,10 +10,6 @@ MiniDeps.now(function()
 		source = 'nvim-treesitter/nvim-treesitter',
 	})
 
-	local isnt_installed = function(lang)
-		return #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0
-	end
-
 	local languages = {
 		'astro',
 		'css',
@@ -35,11 +31,7 @@ MiniDeps.now(function()
 		'wgsl',
 	}
 
-	local to_install = vim.tbl_filter(isnt_installed, languages)
-
-	if #to_install > 0 then
-		require('nvim-treesitter').install(to_install)
-	end
+	require('nvim-treesitter').install(languages)
 
 	local filetypes = {}
 	for _, lang in ipairs(languages) do
@@ -53,7 +45,6 @@ MiniDeps.now(function()
 			vim.treesitter.start(ev.buf)
 		end,
 		desc = 'start tree-sitter',
-		group = vim.api.nvim_create_augroup('start-tree-sitter', {}),
 		pattern = filetypes,
 	})
 end)
